d836507d69b57ac1fe760db9a8d2d80371b10027
Gregory Boddin <gregory@siwhine.net>
2015-11-08 18:45:32 +0100

    Flexible cURL settings for broken SSL install (testing)

--- tests/iTopObjectTest.php
+++ tests/iTopObjectTest.php
@@ -7,7 +7,9 @@ class iTopObjectTest extends PHPUnit_Framework_TestCase
         if(is_null(self::$iTopInstance)) {
             self::$iTopInstance =
                 new iTopApi\iTopClient('https://demo.combodo.com/simple/','admin','admin');
-            self::$iTopInstance->setCertificateCheck(false)
+            // switch SSLv3 if available ... ( broken curl )
+            if(defined('CURL_SSLVERSION_SSLv3'))
+                self::$iTopInstance->setCertificateCheck(false)
                 ->setCurlOption(CURLOPT_SSLVERSION,CURL_SSLVERSION_SSLv3);
         }
         return self::$iTopInstance;
