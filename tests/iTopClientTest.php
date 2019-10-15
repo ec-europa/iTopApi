961b2ca39645dfe90dc556b50f3a9ca8a1ce7937
Sébastien GERVAIS <gervais.seb@gmail.com>
2019-10-15 09:56:58 +0000

    Up iTopClientTest.php

--- tests/iTopClientTest.php
+++ tests/iTopClientTest.php
@@ -6,8 +6,9 @@ class iTopClientTest extends PHPUnit_Framework_TestCase
 
     static function getItopInstance() {
         if(is_null(self::$iTopInstance)) {
+            $itopURL = (getenv ("ITOP_URL") !== FALSE )? getenv ("ITOP_URL") : "http://127.0.0.1";
             self::$iTopInstance =
-                new iTopApi\ITopClient('http://itop:80','admin','admin');
+                new iTopApi\ITopClient($itopURL,'admin','admin');
         }
         return self::$iTopInstance;
     }
