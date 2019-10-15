74976b0c637e9cddcf9a468b6904e010f7610da9
Sébastien GERVAIS <gervais.seb@gmail.com>
2019-10-15 12:23:11 +0000

    up

--- tests/iTopClientTest.php
+++ tests/iTopClientTest.php
@@ -9,6 +9,10 @@ class iTopClientTest extends PHPUnit_Framework_TestCase
             $itopURL = (getenv ("ITOP_URL") !== FALSE )? getenv ("ITOP_URL") : "http://127.0.0.1:80";
             self::$iTopInstance =
                 new iTopApi\ITopClient($itopURL,'admin','admin');
+            // switch SSLv3 if available ... ( broken curl )
+            if(defined('CURL_SSLVERSION_SSLv3'))
+                self::$iTopInstance->setCertificateCheck(false)
+                    ->setCurlOption(CURLOPT_SSLVERSION,CURL_SSLVERSION_SSLv3);
         }
         return self::$iTopInstance;
     }
